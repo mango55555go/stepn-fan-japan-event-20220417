55e119283f5b0b4e9612916225bea6839a8649a5
mango55555go <mango55555go@gmail.com>
2022-04-18 23:42:45 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,27 @@
+## 概要
+2022年4月17日に行われた `STEPN Fan Japanイベント` 参加者が参加賞としてNFTを取得できるサイト
+## 技術スタック
+- Next.js
+- Tailwind CSS
+- Supabase
+  - Firebaseに代わるBaaS
+- Vercel
+  - Hosting
+- TwitterAPI
+  - TwitterIDのみを使用し、その他の情報は取得はできるが利用はしていない
+
+
+## 環境構築
+
+- `.env.local` ファイルを作成（中身はmangoからDMします）
+
+```bash
+npm install
+
+npm run dev
+```
+
+- http://localhost:3000 でアプリで起動を確認
+
+## コントリビューション
+- mainブランチから作業ブランチを作成し、コミット後PR作成をしてください
---
+++ pages/index.js
@@ -0,0 +1,84 @@
+import { useState, useEffect } from "react"
+import { supabase } from "../utils/supabaseClient"
+import Image from "next/image"
+
+export default function Home() {
+  const [session, setSession] = useState(null)
+  const [url, setUrl] = useState('')
+
+  useEffect(() => {
+    setSession(supabase.auth.session())
+
+    supabase.auth.onAuthStateChange((_event, session) => {
+      setSession(session)
+    })
+  }, [])
+
+  getMintingUrl()
+
+  async function getMintingUrl() {
+    try {
+      const user = supabase.auth.user()
+
+      if (!user) return
+
+      let { data, error, status } = await supabase
+        .from('user-mint-url')
+        .select('url')
+        .eq('username', user.user_metadata.user_name)
+        .single()
+
+
+      if (data) {
+        setUrl(data.url)
+      }
+    } catch (error) {
+      alert(error.message)
+    }
+  }
+
+  async function signInWithTwitter() {
+    await supabase.auth.signIn({provider: "twitter"})
+  }
+
+  return (
+    <div className="p-10 flex flex-col items-center justify-center w-screen h-screen bg-[#D33943] text-white">
+      {session ? (
+        <>
+          <Image
+            width={100}
+            height={100}
+            src={session?.user?.user_metadata?.picture}
+            className="rounded-full"
+          />
+          <h2 className="mt-3 text-lg">
+            Welcome {session?.user?.user_metadata?.full_name}
+          </h2>
+          <h2 className="mt-3 text-lg">
+            You are signed in as @{session?.user?.user_metadata?.user_name}
+          </h2>
+
+          {url && (
+            <h2 className="mt-3 text-lg">
+              Your minting site is <a className="text-sky-500" href={url} target="_blank">{url}</a>
+            </h2>
+          )}
+
+          <button
+            className="px-4 py-2 mt-3 text-black bg-white rounded-lg"
+            onClick={() => supabase.auth.signOut()}
+          >
+            Sign out
+          </button>
+        </>
+      ) : (
+        <button
+          onClick={signInWithTwitter}
+          className="relative px-4 py-2 bg-[#1eb872] rounded-lg mt-4"
+        >
+          Twitter sign in
+        </button>
+      )}
+    </div>
+  )
+}
